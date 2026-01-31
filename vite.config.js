71e12761b1de3569dce527f21bb81784e9da34f7
akelloharrison <akelloharrison@gmail.com>
2026-01-31 04:55:28 +0300

    Fix asset paths for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/greenagriwatch/', // Correct base path for GitHub Pages
 });
